ab5e6708cddde20c2c256b71901f6c84588ea0a7
Felix Morgner <felix.morgner@gmail.com>
2018-11-26 08:22:30 +0100

    cmake: check for conan

--- cmake/Modules/ConanDependencies.cmake
+++ cmake/Modules/ConanDependencies.cmake
@@ -5,5 +5,6 @@ endif()
 
 include("${CMAKE_BINARY_DIR}/conan.cmake")
 
+conan_check(REQUIRED)
 conan_add_remote(NAME "bincrafters" URL "https://api.bintray.com/conan/bincrafters/public-conan")
 conan_cmake_run(CONANFILE "conanfile.py" BASIC_SETUP CMAKE_TARGETS BUILD "missing" OUTPUT_QUIET)
